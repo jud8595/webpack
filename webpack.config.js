57d83f8c56be8f60b9d54377cbc9504cc289bca3
jdeshayes <jdeshayes.ext@galerieslafayette.com>
2019-05-20 14:09:47 +0200

    adding HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const webpack = require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let config = {
     entry: {
         app: "./src/index.js",
         print: "./src/print.js"
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
       path: path.resolve(__dirname, "./public"),
       filename: "[name].bundle.js"
